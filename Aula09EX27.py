d8d76230be0aedd34f4e2b9bc93d7fa7670420e1
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-01 22:36:13 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

--- Aula09EX27.py
+++ Aula09EX27.py
@@ -2,6 +2,6 @@
 Faca um Programa que leia o completo de uma pessoa e mostre em seguida o primeiro e o ultimo nome separadamente
 """
 
-#nome = input("Digite seu Nome Completo: ")
-nome = 'Helber Aparecido de Oliveira'
-print("Boa Tarde, {}. Tenha uma bom Dia Aqui {}".format(nome.split(), nome))
+nome = str(input("Digite seu Nome Completo: ")).strip().upper().split()
+#nome = 'Helber Aparecido de Oliveira'
+print("Boa Tarde, {}. Tenha uma bom Dia Sr. {}".format(nome[0], nome[len(nome)-1]))
