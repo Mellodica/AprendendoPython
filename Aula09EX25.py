fbb655133f155efa82dd30ad225b33cc1c186222
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-01 22:08:37 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

--- Aula09EX25.py
+++ Aula09EX25.py
@@ -2,6 +2,6 @@
 Crie um programa que leia o nome de uma pessoa e diga se ela tem silva no nome
 """
 
-nome = str(input("Digite o Seu Nome"))
+nome = str(input("Digite o Seu Nome: ")).strip()
 
-print("SILVA" in nome)
+print("SILVA" in nome.upper())
