9dd835a4727744ccc09140d2de15387a0ffc1281
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-01 21:57:41 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

--- Aula09EX23.py
+++ Aula09EX23.py
@@ -3,12 +3,23 @@
 
 """
 
-num = (input("Digite um Numero: "))
+num = int(input("Digite um Numero: "))
+num1 = str(num)
 
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
 
-print("Unidade {}".format(num[3]))
-print("Dezena {}".format(num[2]))
-print("Centena {}".format(num[1]))
-print("Milhar {}".format(num[0]))
+#print("Unidade {}".format(num1[3]))
+#print("Dezena {}".format(num1[2]))
+#print("Centena {}".format(num1[1]))
+#print("Milhar {}".format(num1[0]))
 
-print("O Valor da Unidade é {} O Valor da Dezena é {}". format(num[3], num[2]))
+#print("O Valor da Unidade é {} O Valor da Dezena é {}". format(num1[3], num1[2]))
+
+print("Unidade {}".format(u))
+print("Dezena {}".format(d))
+print("Centena {}".format(c))
+print("Milhar {}".format(m))
+
