0fb9dc77abfdcf31f0b16208ba583e98c3d96e3a
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-01 22:17:21 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

--- Aula09EX26.py
+++ Aula09EX26.py
@@ -5,8 +5,8 @@ B) Em que posição ela aparece a primeira vez?
 C) Em que posição ela aparece pela a ultima vez?
 """
 
-frase = input("Digite uma Frase: ")
+frase = str(input("Digite uma Frase: ")).strip().upper()
 
-print("Existe na Frase {} Letras 'A'".format(frase.count('a')))
-print("A Letra 'A' aparece a primeira vez no Indice {}".format(frase.find('a')))
-print("A Ultima vez que a Letra 'A' aparece é no indice {}".format(frase.rfind('a')))
+print("Existe na Frase {} Letras 'A'".format(frase.count('A')))
+print("A Letra 'A' aparece a primeira vez no Indice {}".format(frase.find('A')+1))
+print("A Ultima vez que a Letra 'A' aparece é no indice {}".format(frase.rfind('A')+1))
