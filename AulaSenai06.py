51aea18fc67b0f55ab01f914e987d6e53810622f
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-08 19:45:40 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

---
+++ AulaSenai06.py
@@ -0,0 +1,12 @@
+
+
+#n1 = 4
+#n2 = 3
+
+n1 = int(input("Digite o primeiro Valor: "))
+n2 = int(input("Digite o segundo Valor: "))
+
+print("O Valor de Soma é {}".format(n1 + n2))
+print("O Valor da Multiplicação é {}".format(n1 * n2))
+print("O Valor da Divisão é {:.2f}".format(n1 / n2))
+print("O Valor de Subtração é {}".format(n1 - n2))
