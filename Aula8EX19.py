8a62adaacd872a72ed6539b85d25644064e3d7ce
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-08 18:56:00 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

--- Aula8EX19.py
+++ Aula8EX19.py
@@ -12,6 +12,7 @@ n3 = str(input("Terceiro Aluno: "))
 
 lista = [n1, n2, n3]
 
+#importar biblioteca toda o código muda para: escolhido = random.choise(lista)
 escolhido = choice(lista)
 
 print("O Aluno escolhido foi {}".format(escolhido))
