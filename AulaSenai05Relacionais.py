031abcf102cdf72360d96bd12c4be4054667d282
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-08 19:25:53 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

---
+++ AulaSenai05Relacionais.py
@@ -0,0 +1,9 @@
+a = 1
+b = 5
+c = 2
+d = 1
+
+print("O Valor de A é maior que C?", a > c)
+print("O Valor de A é diferente de D?", a != d)
+print("O Valor de D é menor ou igual ao Valor de A?", d <= a)
+print("O Valor de C é diferente do valor de C", c != c)
