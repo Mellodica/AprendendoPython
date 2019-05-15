e1436ca390063453da06fe4dfca4f12f3d065595
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-15 19:10:05 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

---
+++ AulaSenai14.py
@@ -0,0 +1,15 @@
+
+
+if True == False:
+    print('É Igual')
+
+else:
+    print('CaiFora')
+
+
+
+print(10 > 3 or 2 == 4)
+print(10 < 3 or 2 == 4)
+print(10 > 3 or 2 == 2)
+print(10 < 3 and 2 == 2)
+print(not 10 < 3)
---
+++ EXEMPLOS.py
@@ -0,0 +1,25 @@
+"""
+
+VALOR = "mARCIO"
+
+print(valor * 10)
+#tipoVariavel
+
+print(type(123))
+print(type('Olá Mundo'))
+
+#Tamanho da Variavel
+print(len('Olá MUndo'))
+
+
+IF
+
+if True == False:
+    print('É Igual)
+
+else:
+    print('Cai Fora')
+
+    
+"""
+
