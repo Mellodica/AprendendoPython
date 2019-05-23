c423fcf07b194ef8d9f8e9da6fc4eee75fb5fc14
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-22 21:00:56 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

---
+++ AulaSenai25.py
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+print("\n DIGITE SUA PRIMEIRA PERGUNTA:\n\t")
+pr1 = str(input("\n").upper())
+
+
+print("\n DIGITE SUA SEGUNDA PERGUNTA! VALENDOOOOOO!\n\t")
+pr2 = str(input("\n").upper())
+
+
+print("\nDIGITE SUA TERCEIRA PERGUNTA!\n\t")
+pr3 = str(input("\n").upper())
+
+print("\n\n\n\n\t\t   VALEEEEENDOOOOO!")
+print(pr1+'\n\n')
+pr01 = str(input("***** Digite [SIM] ou [NAO] *****\n\t\t").upper())
+
+if pr01 == 'SIM':
+    r1 = 2
+else:
+    r1 = 0
+
+
+print("\n PERGUNTA DOIS!")
+print(pr2+'\n\n')
+pr02 = str(input("***** Digite [SIM] ou [NAO] *****\n\t\t").upper())
+
+if pr02 == 'SIM':
+    r2 = 2
+else:
+    r2 = 0
+
+
+print("\n PERGUNTA TRÊS!!")
+print(pr3+'\n\n')
+pr03 = str(input("***** Digite [SIM] ou [NAO] *****\n\t\t").upper())
+
+if pr03 == 'NAO':
+    r3 = 2
+else:
+    r3 = 0
+
+
+soma = r3 + r1 + r2
+
+
+
+print("Cada Pergunta tem o Valor de 2 Pontos sua pontuação foi {}".format( soma))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
