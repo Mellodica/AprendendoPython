bc8574a1a5951aeab3e6c02ee56f84c4534b40f7
Helber Oliveira <mellodica.louise@gmail.com>
2019-05-29 19:52:22 -0300

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Helber Oliveira <mellodica.louise@gmail.com>

--- AulaSenai32.py
+++ AulaSenai32.py
@@ -2,7 +2,7 @@
 g) Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando
 as seguintes formulas:
 Para Homens (72.7*h)-58
-Para Muleheres (62.1*h)44.7
+Para Muleheres (62.1*h)-44.7
 """
 
 h = float(input('Digite qual sua altura: '))
@@ -11,4 +11,4 @@ if s == 'M':
     print('O peso ideial é {}'.format((62.1*h)-44.7))
 
 if s == 'H':
-    print('O peso ideal é {}'.format((72.7*h)-58))
+    print('O peso ideal é {:.0f} quilos'.format((72.7*h)-58))
